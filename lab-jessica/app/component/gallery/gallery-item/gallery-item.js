2b27f8450fd59a71b2705b4c3571bcba4c9acee7
Jessica Soltero <jmichellevs@gmail.com>
2017-03-06 16:41:38 -0800

    Moving over files from lab33

---
+++ lab-jessica/app/component/gallery/gallery-item/gallery-item.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+  template: require('./gallery-item.html'),
+  controller: ['$log', 'galleryService', GalleryItemController],
+  controllerAs: 'galleryItemCtrl',
+  bindings: {
+    gallery: '<'
+  }
+};
+
+function GalleryItemController($log, galleryService) {
+  $log.debug('GalleryItemController');
+
+  this.showEditGallery = false;
+
+  this.deleteGallery = function() {
+    $log.log('deleteGallery()');
+
+    galleryService.deleteGallery(this.gallery._id);
+  };
+}
